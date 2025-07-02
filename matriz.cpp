c86dfef79570e052ff17ee7dd4455f0edc4466b7
Caio Ongaro <caioongaro@gmail.com>
2025-07-02 01:20:37 -0300

    Adiciona multiplicação de matrizes ao módulo anterior

--- matriz.cpp
+++ matriz.cpp
@@ -26,6 +26,14 @@ int main() {
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 C[i][j] = A[i][j] - B[i][j];
+    } else if (op == 'm') {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                C[i][j] = 0;
+                for (int k = 0; k < n; k++)
+                    C[i][j] += A[i][k] * B[k][j];
+            }
+        }
     } else {
         cout << "Operação inválida para esta versão" << endl;
         return 1;
